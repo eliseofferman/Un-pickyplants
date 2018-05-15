a5438b179510eb48aeedfa3833c837ac6c4dba8d
Johanna <johannahokstrand@gmail.com>
2018-05-15 14:51:35 +0200

    Började med server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import cors from "cors"
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(cors())
+
+mongoose.connect("mongodb://localhost/green-thumb", { useMongoClient: true })
+
+mongoose.Promise = Promise
+
+mongoose.connection.on("error", err => console.error("Connection error:", err))
+mongoose.connection.once("open", () => console.log("Connection to mongodb"))
 
 app.get("/", (req, res) =>
   res.send("Hello World!")
