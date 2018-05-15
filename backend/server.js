344ed3d452ba27f1f0c1c9ec493546ca627219a8
elise offerman <eliseofferman@hotmail.com>
2018-05-15 15:15:39 +0200

    made model and conected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,28 @@ mongoose.Promise = Promise
 mongoose.connection.on("error", err => console.error("Connection error:", err))
 mongoose.connection.once("open", () => console.log("Connection to mongodb"))
 
-app.get("/", (req, res) =>
+const Plant = mongoose.model("Plant", {
+  id: String,
+  name: String
+  // description: String,
+  // waterAmount: String,
+  // color: String,
+  // maintainence: String,
+  // nutrient: String,
+  // soil: String
+})
+
+app.get("/", (req, res) => {
   res.send("Hello World!")
-)
+})
+
+app.post("/", (req, res) => {
+  const plant = new Plant(req.body)
+
+  plant.save()
+    .then(() => { res.status(201).send("Plant created") })
+    .catch(err => { res.status(400).send(err) })
+})
 
 app.listen(8080, () =>
   console.log("Example app listening on port 8080!")
