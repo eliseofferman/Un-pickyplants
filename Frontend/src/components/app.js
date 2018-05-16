616fe5259df0f03abc8993553f8d4f4de4c1bfb5
Johanna <johannahokstrand@gmail.com>
2018-05-16 10:29:47 +0200

    New props with state to App

--- Frontend/src/components/app.js
+++ Frontend/src/components/app.js
@@ -8,11 +8,29 @@ import PlantItem from "./plant-item"
 
 class App extends React.Component {
 
+  state = {
+    inputSunx: "",
+    inputWater: "",
+    inputSun: ""
+
+  }
+  // updateStateSun = (event) => {
+  //   this.setState ({ inputSun: event })
+  // }
+  //
+  // updateStateWater = (event) => {
+  //   this.setState ({ inputWater: event })
+  // }
+
   render() {
     return (
       <div>
         <Hero />
-        <Input />
+        <Input
+          // callbackStateSun={this.updateStateSun}
+          // callbackStateWater={this.updateStateWater}
+          // state={this.state}
+        />
         <Recomendation />
         <PlantsList />
         <PlantItem />
--- Frontend/src/components/input.js
+++ Frontend/src/components/input.js
@@ -1,39 +1,69 @@
 import React from "react"
 
 class Input extends React.Component {
-  state = {
-    inputSun: " ",
-    inputWater: " "
-  }
-   onChangeSun = (event) => {
-     this.setState ({ inputSun: event.target.value })
-   }
 
-   onChangeWater = (event) => {
-     this.setState ({ inputWater: event.target.value })
-   }
+  const sunAmount = [
+    "in window",
+    "in wardrobe",
+    "in room"
+  ]
+
+    const waterAmount = [
+      "once a week",
+      "once a month",
+      "once a year"
+    ]
+
+
+   // onChangeSun = (event) => {
+   //   this.props.callbackStateSun(event.target.value)
+   // }
+   //
+   // onChangeWater = (event) => {
+   //   this.props.callbackStateWater(event.target.value)
+   // }
 
   render() {
    return (
       <div>
         <label>
+          <h3>Choose sun amount </h3>
+             <select>
+            {sunAmount.map(option => (
+              <option
+                selected={option === this.props.xxx}
+                key={option}
+                value={option}
+              </option>
+                ))}
+            </select>
+
+          </label>
+
+
+
+
+
+
+        {/* <label onSubmit=>
           <form>
             <input type="text"
-
-            value= {this.state.inputSun}
+             value= {this.props.state.inputSun}
             onChange= {this.onChangeSun} />
             </form>
 
-         <form>
+          <form>
          <input type="text"
-           value= {this.state.inputWater}
+           value= {this.props.state.inputWater}
            onChange= {this.onChangeWater} />
          </form>
 
+       <button>
+         Find unpicky plant!
+       </button>
 
 
-
-      </label>
+      </label> */}
       </div>
     )
   }
