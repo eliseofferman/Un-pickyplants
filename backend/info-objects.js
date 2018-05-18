4282930e917354a07c6c52ffa21a4dfe0cd472cf
elise offerman <eliseofferman@hotmail.com>
2018-05-18 11:03:38 +0200

    addad api text info

---
+++ backend/info-objects.js
@@ -0,0 +1,184 @@
+{
+  "name": "Monstera",
+
+  "id": "Monstera",
+
+  "color": "Green",
+
+  "imageFileName": "monstera.jpg",
+
+  "description": "Monstera has become of the most populare indoor plant in the recent years, and it is easy to understand why! With it’s big and broad leaf, and it’s very un-picky character this plant is very easy to maintaine",
+
+  "waterAmount": ["Dried out between watering the plant"],
+
+  "sunAmount": ["Partly shadow", "Shadow" ],
+
+  "maintainence": "Mostera likes humidty, and to have it’s leafe sprayed with water. One tip is to shower the leafs once a week!",
+
+  "nutrient": "Add nutrient every second week",
+
+  "soil": "Planting soil",
+
+  "location": "Avoid direct sunlight, so place the plant futher in in the room"
+}
+
+{
+  "name": "Snake plant",
+
+  "id": "Snake plant",
+
+  "color": "Green",
+
+  "imageFileName": "snake-plant.jpg",
+
+  "description": "This plant rarely get’s infectede with disease or with pests, whitch is a big advantage! Also this plant is nearly impossible to kill, the only threat is over watering",
+
+  "waterAmount": ["Dried out between watering the plant"],
+
+  "sunAmount": ["Partly shadow", "Shadow", "Direct sunlight"],
+
+  "maintainence": "This plant needs literally no maintaninence. This plant prefers to have its soil completely dried out between watering",
+
+  "nutrient": "Add nutrient every second week, during spring and fall",
+
+  "soil": "Sandy soil",
+
+  "location": "Anywhere is fine, but to preserve it’s beautiful green color place it in a sunny place",
+}
+
+{
+  "name": "Golden Pothos",
+
+  "id": "Golden Pothos",
+
+  "color": "Green",
+
+  "imageFileName": "golden-pothos.jpg",
+
+  "description": "This plant likes humdity, and it’s leaf is beautiful heart shaped. Golden Pothos can vary in color, from light-green to white with beautiful patterns on it’s leaf. This plant likes the soil to be evenly moist. But don’t water to much!",
+
+  "waterAmount": ["Once a week"],
+
+  "sunAmount": ["Partly shadow"],
+
+  "maintainence": "Golden Pothos is easy to maintatin, but keep in mind the plant likes it’s soil moist. Golden Pothos its beautiful hanging green leafs fits it good ampel"
+
+  "nutrient": "Add a small dose nutrient during spring and fall, and in winter time once a month",
+
+  "soil": "Planting soil",
+
+  "location": "To preserve the plants beautiful color, place the plant in a bright spot but not in direct sunlight"
+}
+
+{
+  "name": "ZZ plant",
+  "id": "ZZ plant",
+  "color": "Green",
+  "imageFileName": "zz-plant.jpg",
+
+  "description": "This plant you can easily forget about, or put in the closet and it will survive!",
+
+  "waterAmount": ["Dried out between watering the plant"],
+
+  "sunAmount": ["Partly shadow, shadow, direct sunlight"],
+
+  "maintainence": "This plant needs literally no maintaninence. This plant prefers to have its soil completely dried out between watering",
+
+  "nutrient": "Add nutrient once a month during summer time",
+
+  "soil": "Planting soil",
+
+  "location": "Anywhere is fine"
+}
+
+{
+  "name": "Kalankoe",
+  "id": "Kalankoe",
+  "color": "Different shades of pink, yellow, orange, red, white",
+  "imageFileName": "kalankoe.jpg",
+  "description": "Kalankoe is easy to maintatin. The plant tolerates to be dried out or stand in a dry air environment",
+
+  "waterAmount": ["Once a week"],
+
+  "sunAmount": ["Direct sunlight", "Partly shadow"],
+
+  "maintainence": "Water once a week with with not to much water. The plasts shallow root system should never stand in water",
+
+  "nutrient": "Potted plant nutrition for flowering plants",
+
+  "soil": "Planting soil",
+
+  "location": "Place in a bright spot, can be standing in direct sunlight"
+}
+
+{
+  "name": "Zebra plant",
+
+  "id": "Zebra plant",
+
+  "color": "Green and purple leaves",
+
+  "imageFileName": "zebra.jpg",
+  
+  "description": "Zebra plant is easy to maintatin. With it's beautiful hanging green and purpel leafs, this plant fits it perfect in a ampel",
+
+  "waterAmount": ["Once a week"],
+
+  "sunAmount": ["Partly shadow"],
+
+  "maintainence": "Water once a week to keep the soil moist. Do not let the let the plant be standing in water",
+
+  "nutrient": "Add nutrient once a month",
+
+  "soil": "Planting soil",
+
+  "location": "Place in a light spot , but not direct sun"
+}
+
+{
+  "name": "Peace lily",
+
+  "id": "Peace lily",
+
+  "color": "Green with white flowers",
+
+  "imageFileName": "peace-lily.jpeg",
+
+  "description": "This plant has long and thick stem, so it needs a wide pot. Peace lily will bloom the winter time",
+
+  "waterAmount": ["Once a week"],
+
+  "sunAmount": ["Direct sunlight", "Partly shadow"],
+
+  "maintainence": "This plant needs a lot of water, so keep it mouist during the whole growing period",
+
+  "nutrient": "Potted plant nutrient for plants",
+
+  "soil": "Planting soil",
+
+  "location": "The plant likes to be places in light shadow, but also in the sun"
+}
+
+{
+  "name": "Cactus",
+
+  "id": "Cactus",
+
+  "color": "Green",
+
+  "imageFileName": "cactus.jpg",
+
+  "description": "Cactus is a plant that is use to survive under hard conditions",
+
+  "waterAmount": ["Once a month", "Dried out between watering the plant"],
+
+  "sunAmount": ["Direct sunlight"],
+
+  "maintainence": "During winter time, give the cactus a minimal amount of water. When spring and summer times comes, give the plant gradually more water",
+
+  "nutrient": "To have a happy cactus, please give it nutrient once a month",
+
+  "soil": "Planting soil",
+
+  "location": "During wintertime, place the plant in a cool and dry place. When spring and summer comes move the plant to a more sunny place"
+}
