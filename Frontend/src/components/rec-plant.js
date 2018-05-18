236008dd299a3a6834967d350552b4a1edf02ef2
elise offerman <eliseofferman@hotmail.com>
2018-05-18 12:13:58 +0200

    added image path for plant

--- Frontend/src/components/rec-plant.js
+++ Frontend/src/components/rec-plant.js
@@ -1,13 +1,15 @@
 import React from "react"
+// import image from "../images/cactus.jpg"
+// import image from {this.props.plantinformation.imageFileName}
 import "./rec-plant.css"
 
 class RecPlant extends React.Component {
   render() {
-
     return (
       <div className="main-box">
         <div className="plant-box">
           <h3>{this.props.plantinformation.name}</h3>
+          <img src={require(`../images/${this.props.plantinformation.imageFileName}`)}  alt="m" />
         </div>
       </div>
     )
