c47fc7dd6f485f6fd09b783f0ae5f63b0f42ce59
Johanna <johannahokstrand@gmail.com>
2018-05-21 15:40:58 +0200

    Plant item images work

--- Frontend/src/components/plant-item.js
+++ Frontend/src/components/plant-item.js
@@ -2,7 +2,7 @@ import React from "react"
 
 class PlantItem extends React.Component {
   state = {
-    plant: ""
+    plant: null
   }
   componentDidMount() {
     fetch(`http://localhost:8080/plants/${this.props.match.params.plantId}`).then(response => (
@@ -13,21 +13,31 @@ class PlantItem extends React.Component {
   }
 
   render() {
-    console.log("test", this.state.plant)
-    return (
+    if(this.state.plant) {
+      
+      return (
 
-      <div className="plant-container">
-        <div className="image-container">
-          {/* <img src={require(`../images/${this.state.plant.imageFileName}`)}  alt="plant image" /> */}
+        <div className="plant-container">
+          <div className="image-container">
+            <img src={require(`../images/${this.state.plant.imageFileName}`)}  alt="plant image" />
+          </div>
+
+          <div className="info-container">
+            <h3>{this.state.plant.name}</h3>
+            <p>Color: {this.state.plant.color}</p>
+            <p>Description: {this.state.plant.description}</p>
+            <p>Maintainence: {this.state.plant.maintainence}</p>
+            <p>Nutrient: {this.state.plant.nutrient}</p>
+            <p>Soil: {this.state.plant.soil}</p>
+            <p>Location: {this.state.plant.location}</p>
+          </div>
         </div>
-        <div className="info-container">
-          <h3>{this.state.plant.name}</h3>
-          <p>Color: {this.state.plant.color}</p>
-          <p>Description: {this.state.plant.description}</p>
-          <p>Maintainence: {this.state.plant.maintainence}</p>
-        </div>
-      </div>
-    )
+      )
+    } else {
+      return(
+        <div>Loading..</div>
+      )
+    }
   }
 }
 
