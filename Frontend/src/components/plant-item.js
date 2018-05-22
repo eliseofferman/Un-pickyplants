5d47f7662e0d3692e919efe57c4a1c5b99d5a8e1
elise offerman <eliseofferman@hotmail.com>
2018-05-22 15:00:06 +0200

    styling bread crums

--- Frontend/src/components/plant-item.js
+++ Frontend/src/components/plant-item.js
@@ -1,6 +1,6 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import PlantItem from "./plant-item.css"
-
 
 class Plant extends React.Component {
   state = {
@@ -14,23 +14,31 @@ class Plant extends React.Component {
     })
   }
 
+  // renderBreadCrumbs = () => {
+  //   const Ho
+  // }
+
   render() {
     if (this.state.plant) {
       return (
-
-        <div className="plant-container">
-          <div className="image-container">
-            <img src={require(`../images/${this.state.plant.imageFileName}`)}  alt="plant image" />
+        <div>
+          <div className="breadCrumb">
+            <Link to="/un-pickyplants"><h3>Main page</h3></Link>
           </div>
+          <div className="plant-container">
+            <div className="image-container">
+              <img src={require(`../images/${this.state.plant.imageFileName}`)}  alt="plant image" />
+            </div>
 
-          <div className="info-container">
-            <h3>{this.state.plant.name}</h3>
-            <p>Color: {this.state.plant.color}</p>
-            <p>Description: {this.state.plant.description}</p>
-            <p>Maintainence: {this.state.plant.maintainence}</p>
-            <p>Nutrient: {this.state.plant.nutrient}</p>
-            <p>Soil: {this.state.plant.soil}</p>
-            <p>Location: {this.state.plant.location}</p>
+            <div className="info-container">
+              <h3>{this.state.plant.name}</h3>
+              <p>Color: {this.state.plant.color}</p>
+              <p>Description: {this.state.plant.description}</p>
+              <p>Maintainence: {this.state.plant.maintainence}</p>
+              <p>Nutrient: {this.state.plant.nutrient}</p>
+              <p>Soil: {this.state.plant.soil}</p>
+              <p>Location: {this.state.plant.location}</p>
+            </div>
           </div>
         </div>
       )
