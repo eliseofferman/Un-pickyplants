e6c0379c3a19ec496a30b4785ff526242394183c
Johanna <johannahokstrand@gmail.com>
2018-05-22 13:34:28 +0200

    hero scss

--- Frontend/src/components/hero.js
+++ Frontend/src/components/hero.js
@@ -6,7 +6,11 @@ class Hero extends React.Component {
 
     return (
       <div className="hero-container">
-        <img className="hero-img" src={require(`../images/hero-kaktus1.jpg`)}  alt="plant image" />  
+        <img className="hero-img" src={require(`../images/hero-kaktus1.jpg`)}  alt="plant image" />
+
+        <div className="hero-info">
+          <h1>Welcome to Un-picky plants</h1>
+        </div>
 
       </div>
     )
