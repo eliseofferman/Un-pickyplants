3326f09551e248c4f487b845ffe5696051b80734
Johanna <johannahokstrand@gmail.com>
2018-05-22 13:20:37 +0200

    hero-img working

--- Frontend/src/components/hero.js
+++ Frontend/src/components/hero.js
@@ -1,11 +1,13 @@
 import React from "react"
+import "./hero.css"
 
 class Hero extends React.Component {
   render() {
 
     return (
-      <div>
-        Hello Hero
+      <div className="hero-container">
+        <img className="hero-img" src={require(`../images/hero-kaktus1.jpg`)}  alt="plant image" />  
+
       </div>
     )
   }
